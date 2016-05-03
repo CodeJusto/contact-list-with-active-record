1f40d45d145638690cf79f007fb4a69ee1c52f29
Justin Wong <justincwong89@gmail.com>
2016-05-03 00:15:16 +0000

    Everything is working, but maybe not after this

--- contact.rb
+++ contact.rb
@@ -4,16 +4,27 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
-  attr_accessor :name, :email
+class IncorrectID < StandardError
+
+end
+
+  attr_reader :id
+  attr_accessor :name, :email, :number
   
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
-  def initialize(name, email, id= nil)
+  def initialize(name, email, full_number, id= nil)
+    raise 'IDs must be greater than 0 (and also numbers)!' if id && id.to_i <= 0
     @name = name
     @email = email
+    @number = full_number
     @id = id
     # TODO: Assign parameter values to instance variables.
+  end
+
+  def generate_id
+    @id = CSV.read("contacts.csv").length+1
   end
 
   # Provides functionality for managing contacts in the csv file.
@@ -22,18 +33,24 @@ class Contact
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # contact_array_csv = CSV.read("contacts.csv")
-      # p @contact_array[2] 
-      # @contact_array[2][0].gsub /\t/, '***'
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
-      CSV.read("contacts.csv").map {|row| Contact.new(row[1],row[2],row[0].to_i) } 
+      # CSV.read("file") returns an array based on each line of the CSV. We are then creating a new 
+      # array using map USING elements of each row from the CSV.
+      CSV.read("contacts.csv").map {|row| Contact.new(row[1],row[2],row[0].to_i)} 
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
-    def create(name, email)
+    def create(name, email, full_number)
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+      final_array = []
+      new_contact = Contact.new(name,email,full_number)
+      CSV.open("contacts.csv", "a") do |csv|
+        raise "That email is already in the system and cannot be created" if search(new_contact.email).empty? == false
+        csv << [new_contact.generate_id, new_contact.name, new_contact.email, new_contact.number].flatten
+      end
+      new_contact
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
@@ -41,6 +58,18 @@ class Contact
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+        # binding.pry
+      # while id && id.to_i <= CSV.read("contacts.csv").length + 1
+        # begin 
+          full_array = CSV.read("contacts.csv").map {|row| Contact.new(row[1],row[2],row[0].to_i)}.select {|contact| contact.id == id.to_i} 
+        # rescue NoMethodError
+        #   puts "There was a problem with the contact. Please re-enter"
+        #   id = gets.chomp
+        # else NoMethodError
+        #   break
+        # end
+      # end
+      full_array[0]
     end
     
     # Search for contacts by either name or email.
@@ -48,6 +77,10 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      full_array = CSV.read("contacts.csv").map {|row| Contact.new(row[1],row[2],row[0])}.select do |contact| 
+        contact.name.include?(term) || contact.email.include?(term)
+      end 
+      full_array
     end
 
   end
--- contact_list.rb
+++ contact_list.rb
@@ -1,4 +1,5 @@
 require_relative 'contact'
+require 'pry'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
@@ -10,17 +11,55 @@ def initialize
     puts "   list    - List all contacts"
     puts "   show    - Show a contact"
     puts "   search  - Search contacts"
+  # Add new contacts
   elsif ARGV[0] == "new"
-    puts "new"
+    puts "Creating new contact"
+    puts "What is the full name?"
+    full_name = STDIN.gets.chomp
+    puts "What is the email?"
+    email = STDIN.gets.chomp
+    input = ""
+    full_number = []
+    while input != "no"
+      puts "Enter the name of your phone."
+      phone_name = STDIN.gets.chomp
+      puts "Enter the number for this phone"
+      phone_number = STDIN.gets.chomp
+      full_number << "#{phone_name}: #{phone_number}"
+      puts "Do you want to create another number? (yes/no)"
+      input = STDIN.gets.chomp.downcase
+    end
+    # binding.pry
+    new_contact = Contact.create(full_name, email, full_number)
+    puts "ID: #{new_contact.id} | Name: #{new_contact.name} | Email: #{new_contact.email} | Phone: #{new_contact.full_number}"
+  # List all contacts
   elsif ARGV[0] == "list"
     puts "Listing all contacts..."
-    p Contact.all
-    # puts "--"
-    # puts "#{Contact.contact_array} records total."
-  elsif ARGV[0] == "show"
-    puts "show"
-  elsif ARGV[0] == "search"
-    puts "search"
+    retrieved_array = Contact.all
+    retrieved_array.each do |line|
+      puts "ID: #{line.id} | Name: #{line.name} | Email: #{line.email}"
+    end
+  # Show a single contact
+  elsif ARGV[0] == "show" && /\d+/.match(ARGV[1]) 
+    retrieved_array = Contact.find(ARGV[1])
+    while true
+      begin
+        puts "ID: #{retrieved_array.id} | Name: #{retrieved_array.name} | Email: #{retrieved_array.email}"
+        break
+      rescue
+        puts "Please enter a new ID"
+        input = STDIN.gets.chomp
+        retrieved_array = Contact.find(input)
+      end
+    end
+  # Search for a single contact
+  elsif ARGV[0] == "search" && /\w+/.match(ARGV[1])
+    retrieved_array = Contact.search(ARGV[1])
+    retrieved_array.each do |contact| 
+      puts "ID: #{contact.id} | Name: #{contact.name} | Email: #{contact.email}" 
+    end
+  else 
+    puts "Command not recognized."
   end
 end
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
