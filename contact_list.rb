d44c94d1610efa388acde41dd1bc8c95f95bc565
Justin Wong <justincwong89@gmail.com>
2016-05-10 21:41:34 +0000

    Completed main objectives

--- contact_list.rb
+++ contact_list.rb
@@ -6,8 +6,6 @@ require 'pry'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-# @counter = 0
-
 def initialize
   if ARGV.empty?
     puts "Here is a list of available commands:"
@@ -17,64 +15,60 @@ def initialize
     puts "   search  - Search contacts"
   # Add new contacts
   elsif ARGV[0] == "new"
-    puts "Creating new contact"
-    puts "What is the full name?"
-    full_name = STDIN.gets.chomp
-    puts "What is the email?"
-    email = STDIN.gets.chomp
-    input = ""
-    full_number = []
-    while input != "no"
-      puts "Enter the name of your phone."
-      phone_name = STDIN.gets.chomp
-      puts "Enter the number for this phone"
-      phone_number = STDIN.gets.chomp
-      full_number << "#{phone_name}: #{phone_number}"
-      # counter += 1
-      puts "Do you want to create another number? (yes/no)"
-      input = STDIN.gets.chomp.downcase
-    end
-    new_contact = Contact.create(full_name, email, full_number)
-    puts "ID: #{new_contact.id} | Name: #{new_contact.name} | Email: #{new_contact.email} | Phone: #{new_contact.number.compact}"
+    contact_info = get_info
+    new_contact = Contact.create(contact_info[0], contact_info[1])  
+    display(new_contact)
 
-  # List all contacts
   elsif ARGV[0] == "list"
     puts "Listing all contacts..."
-    puts "GETTING HERE!"
-    retrieved_array = Contact.all
-    counter = 0
-    retrieved_array.each do |line| 
-      if (counter % 5 == 0) && (counter > 0)
-        STDIN.gets
-      end
-        puts "ID: #{line.id} | Name: #{line.name} | Email: #{line.email} | Phone: #{line.number.compact}"
-        counter += 1
+    contacts = Contact.all
+    contacts.each do |contact|
+      display(contact)
     end
-  # Show a single contact
+
   elsif ARGV[0] == "show" && /\d+/.match(ARGV[1]) 
-    retrieved_array = Contact.find(ARGV[1])
-    # while true
-    #   begin
-        puts "ID: #{retrieved_array.id} | Name: #{retrieved_array.name} | Email: #{retrieved_array.email} | Phone: #{retrieved_array.number.compact}"
-    #     break
-    #   rescue
-    #     puts "Please enter a new ID"
-    #     input = STDIN.gets.chomp
-    #     retrieved_array = Contact.find(input)
-    #   end
-    # end
-  # Search for a single contact
+    puts "Finding specified contact..."
+    test = Contact.find(ARGV[1])
+    display(test[0])
+
   elsif ARGV[0] == "search" && /\w+/.match(ARGV[1])
-    retrieved_array = Contact.search(ARGV[1])
-    retrieved_array.each do |contact| 
-      puts "ID: #{contact.id} | Name: #{contact.name} | Email: #{contact.email} | Phone: #{contact.number.compact}" 
+    puts "Searching"
+    identified = Contact.search(ARGV[1])
+    identified.each do |contact|
+      display(contact)
     end
+
+  elsif ARGV[0] == "update" && /\d+/.match(ARGV[1]) 
+    id = ARGV[1]
+    contact = Contact.find(id)[0]
+    input = get_info
+    contact.name = input[0]
+    contact.email = input[1]
+    contact.save
+    display(contact)
+
+  elsif ARGV[0] == "destroy" && /\d+/.match(ARGV[1]) 
+    id = ARGV[1]
+    the_contact = Contact.find(id)
+    the_contact[0].destroy
+    puts "Contact destroyed."
   else 
     puts "Command not recognized."
   end
 end
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+  def display(contact)
+    puts "ID: #{contact.id}, Name: #{contact.name}, Email: #{contact.email}"
+  end
+
+  def get_info
+    info = []
+    puts "What is the full name?"
+    info << STDIN.gets.chomp
+    puts "What is the email?"
+    info << STDIN.gets.chomp
+    info
+  end
 end
 
 session = ContactList.new
